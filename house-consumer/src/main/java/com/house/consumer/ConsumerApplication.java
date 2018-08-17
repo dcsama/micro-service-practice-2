bfa4931ffe93a4911db244b06079e99bf2027ddb
Hikari <dcsama1218@126.com>
2018-08-17 15:56:27 +0800

    Hystrix 监控
    Hystrix 监控

--- house-consumer/src/main/java/com/house/consumer/ConsumerApplication.java
+++ house-consumer/src/main/java/com/house/consumer/ConsumerApplication.java
@@ -1,10 +1,13 @@
 package com.house.consumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,5 +17,17 @@ public class ConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(
+                streamServlet, "/actuator/hystrix.steam");
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.setName("hystrixMetricsStreamServlet");
+
+        return registrationBean;
     }
 }
